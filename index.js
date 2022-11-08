c1041406de84267dddf134e104f39ca8cb67e66e
rahmanabdulquadir <rahmanaq777@gmail.comm>
2022-11-08 20:35:24 +0600

    basic node mongo setup

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
-const express = require('express')
-const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express()
-const port = process.env.PORT || 5000
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
 
-// middle wares 
-app.use(cors())
-app.use(express.json())
+// middle wares
+app.use(cors());
+app.use(express.json());
 
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.rl1a5mc.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rl1a5mc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
   client.close();
 });
 
-
-app.get('/', (req, res) => {
-  res.send('online delivery service server is running')
-})
-
+app.get("/", (req, res) => {
+  res.send("online delivery service server is running");
+});
 
 app.listen(port, () => {
-  console.log(`server running succesfully ${port}`)
-})
+  console.log(`server running succesfully ${port}`);
+});
