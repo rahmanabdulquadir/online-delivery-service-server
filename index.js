454da4b1bcdce1805ed164289708c462d008a9a8
rahmanabdulquadir <rahmanaq777@gmail.comm>
2022-11-09 20:12:36 +0600

    api updated

--- index.js
+++ index.js
@@ -30,6 +30,12 @@ async function run(){
       const services = await cursor.toArray()
       res.send(services)
     })
+    app.get('/services3', async(req, res) => {
+      const query = {}
+      const cursor = serviceCollection.find(query)
+      const services = await cursor.limit(3).toArray()
+      res.send(services)
+    })
 
     app.get('/services/:id', async (req, res) => {
       const id = req.params.id;
