7576996c3de375e79ccc85e512d64302f597ff53
rahmanabdulquadir <rahmanaq777@gmail.comm>
2022-11-09 23:19:49 +0600

    updated api added

--- index.js
+++ index.js
@@ -9,61 +9,81 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rl1a5mc.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rl1a5mc.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
 
-async function run(){
-  try{
-    const serviceCollection = client.db('deliverService').collection('services')
-    const reviewCollection = client.db('deliverService').collection('reviews')
+async function run() {
+  try {
+    const serviceCollection = client
+      .db("deliverService")
+      .collection("services");
+    const reviewCollection = client.db("deliverService").collection("reviews");
 
-    app.get('/services', async(req, res) => {
-      const query = {}
-      const cursor = serviceCollection.find(query)
-      const services = await cursor.toArray()
-      res.send(services)
-    })
-    app.get('/services3', async(req, res) => {
-      const query = {}
-      const cursor = serviceCollection.find(query)
-      const services = await cursor.limit(3).toArray()
-      res.send(services)
-    })
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+    app.get("/services3", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.limit(3).toArray();
+      res.send(services);
+    });
 
-    app.get('/services/:id', async (req, res) => {
+    app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const service = await serviceCollection.findOne(query);
       res.send(service);
-  });
+    });
 
     // reviews api
-    app.get('/reviews', async(req, res) => {
-      console.log(req.query)
-      const query = {}
-      const cursor = reviewCollection.find(query)
-      const reviews = await cursor.toArray()
-      res.send(reviews)
-    })
-    app.post('/reviews', async(req, res) => {
+    app.get("/reviews", async (req, res) => {
+      let query = {};
+      if (req.query.email) {
+        query = {
+          email: req.query.email,
+        };
+      }
+      const cursor = reviewCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
+    app.post("/reviews", async (req, res) => {
       const review = req.body;
-      const result = await reviewCollection.insertOne(review)
-      res.send(result)
-    })
-  }
-  finally{
+      const result = await reviewCollection.insertOne(review);
+      res.send(result);
+    });
 
-  }
+    app.patch("/reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const status = req.body.status;
+      const query = { _id: ObjectId(id) };
+      const updatedRev = {
+        $set: {
+          status: status,
+        },
+      };
+      const result = await reviewCollection.updateOne(query, updatedRev);
+      res.send(result);
+    });
 
+    app.delete("/reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reviewCollection.deleteOne(query);
+      res.send(result);
+    });
+  } finally {
+  }
 }
-run().catch(err => console.error(err))
+run().catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
   res.send("online delivery service server is running");
