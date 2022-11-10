3fc488284abfa148d50b089a38550088458856ed
rahmanabdulquadir <rahmanaq777@gmail.comm>
2022-11-10 20:47:49 +0600

    api added for jwt token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
@@ -22,6 +23,12 @@ async function run() {
       .db("deliverService")
       .collection("services");
     const reviewCollection = client.db("deliverService").collection("reviews");
+
+    app.post('/jwt', (req,res) => {
+      const user = req.body
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+      res.send({token})
+    })
 
     app.get("/services", async (req, res) => {
       const query = {};
